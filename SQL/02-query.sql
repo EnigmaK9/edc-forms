4256f48df3d086b220f4acc8f9f11d32a3f3224f
EnigmaK9 <enigmak9@protonmail.com>
2024-06-03 11:00:46 -0600

    updated my repo. added python code

--- SQL/02-query.sql
+++ SQL/02-query.sql
@@ -1,4 +1,5 @@
+-- Consulta de lo que va del anio, solo de las compradoras
 select c.name, p.purchase_date, p.amount
 from purchases p
-join customers c on p.customer_id = c.customer_id
+join clients c on p.client_id = c.client_id
 where c.gender = 'f' and year(p.purchase_date) = year(curdate());
